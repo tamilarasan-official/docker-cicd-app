83b0106cc5476354d9ef42f908cedde02f8d5cfd
tamilarasan-official <your-email@example.com>
2026-04-29 13:48:34 +0530

    Change port from 5001 to 3000

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return "Welcome to Docker CI/CD Pipeline! This application is running with GitHub Actions automation, Docker containerization, and automated deployment from DockerHub. Build once, deploy everywhere!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=3000)
