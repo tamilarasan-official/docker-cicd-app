57f0ad3e47d81cfe9fdfd0087fa12577579faa2a
tamilarasan-official <your-email@example.com>
2026-04-29 13:51:13 +0530

    Update return statement to shorter message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Docker CI/CD Pipeline! This application is running with GitHub Actions automation, Docker containerization, and automated deployment from DockerHub. Build once, deploy everywhere!"
+    return "Docker CI/CD App - Running on Port 3000! ✅"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
